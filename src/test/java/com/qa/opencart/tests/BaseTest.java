f2a87690b9d9a1804b70b5d100ed0b67bcff1289
Kumar <ashish.ek.kumar@accenture.com>
2024-01-01 12:18:04 +0530

    my first commit for POM code

---
+++ src/test/java/com/qa/opencart/tests/BaseTest.java
@@ -0,0 +1,51 @@
+package com.qa.opencart.tests;
+
+import java.util.Properties;
+
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Parameters;
+import org.testng.asserts.SoftAssert;
+
+import com.qa.opencart.factory.DriverFactory;
+import com.qa.opencart.pages.AccountsPage;
+import com.qa.opencart.pages.LoginPage;
+import com.qa.opencart.pages.ProductInfoPage;
+import com.qa.opencart.pages.RegistrationPage;
+import com.qa.opencart.pages.SearchResultsPage;
+
+public class BaseTest {
+
+	DriverFactory df;
+	Properties prop;
+	WebDriver driver;
+	protected LoginPage loginPage;
+	protected AccountsPage accountsPage;
+	protected SearchResultsPage searchResultPage;
+	protected ProductInfoPage productInfoPage;
+	protected RegistrationPage registrationPage;
+	SoftAssert softAssert;
+
+	@Parameters({ "browser" })
+
+	@BeforeTest
+	public void setup(String browserName) {
+		df = new DriverFactory();
+		prop = df.init_prop();
+
+		if (browserName != null) {
+			prop.setProperty("browser", browserName);
+		}
+
+		driver = df.init_driver(prop);
+		loginPage = new LoginPage(driver);
+		softAssert = new SoftAssert();
+	}
+
+	@AfterTest
+	public void tearDown() {
+		driver.quit();
+	}
+
+}
